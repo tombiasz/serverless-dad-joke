c39d06db62304888c0196efa6427b747914f9f08
Tomasz Nowacki <tomasz.nowacki@netguru.co>
2018-12-06 10:37:44 +0100

    post joke to twitter

--- src/dadJoke/getDadJokeMethod.js
+++ src/dadJoke/getDadJokeMethod.js
@@ -1,16 +1,24 @@
 const { Method } = require('../common/method');
 const { SuccessResponseObject } = require('../common/successResponseObject');
 const { DadJokeService } = require('./dadJokeService');
+const { TwitterService } = require('./twitterService');
 
 class GetDadJokeMethod extends Method {
-  constructor(dadJokeService = new DadJokeService()) {
+  constructor(
+    dadJokeService = new DadJokeService(),
+    twitterService = TwitterService.buildFromEnvVars(process.env),
+  ) {
     super();
     this.dadJokeService = dadJokeService;
+    this.twitterService = twitterService;
   }
 
-  async processRequest(request) {
+  async processRequest() {
     const joke = await this.dadJokeService.getJoke();
-    return new SuccessResponseObject(joke);
+    const jokeFormatted = this.dadJokeService.formatForTwitter(joke);
+    const tweet = await this.twitterService.postTweet(jokeFormatted);
+    const link = this.twitterService.buildLinkToTweet(tweet);
+    return new SuccessResponseObject({ link, joke: jokeFormatted });
   }
 }
 
--- tests/dadJoke/getDadJokeMethod.test.js
+++ tests/dadJoke/getDadJokeMethod.test.js
@@ -1,21 +1,52 @@
 const { SuccessResponseObject } = require('../../src/common/successResponseObject');
 const { GetDadJokeMethod } = require('../../src/dadJoke/getDadJokeMethod');
 
+const joke = 'a joke';
+const jokeFormatted = `${joke} formatted`;
 const dadJokeService = {
-  getJoke: jest.fn().mockReturnValue('a joke'),
+  getJoke: jest.fn(() => Promise.resolve(joke)),
+  formatForTwitter: jest.fn().mockReturnValue(jokeFormatted),
+};
+
+const screenName = 'foobar';
+const tweetId = '23123123';
+const linkToTweet = 'https://link';
+const tweet = { id_str: tweetId, user: { screen_name: screenName } };
+
+const twitterService = {
+  postTweet: jest.fn(() => Promise.resolve(tweet)),
+  buildLinkToTweet: jest.fn().mockReturnValue(linkToTweet),
 };
 
 describe('GetDadJokeMethod', () => {
+  let getDadJokeMethod = null;
+  beforeEach(() => {
+    getDadJokeMethod = new GetDadJokeMethod(dadJokeService, twitterService);
+  });
+
   test('should return successful response', async () => {
-    const getDadJokeMethod = new GetDadJokeMethod(dadJokeService);
     const response = await getDadJokeMethod.process();
     expect(response.type).toBe(SuccessResponseObject.SUCCESS);
-    expect(response.value).toBe('a joke');
+    expect(response.value).toEqual({ joke: jokeFormatted, link: linkToTweet });
   });
 
   test('should get joke from service', async () => {
-    const getDadJokeMethod = new GetDadJokeMethod(dadJokeService);
     await getDadJokeMethod.process();
     expect(dadJokeService.getJoke).toHaveBeenCalledWith();
+  });
+
+  test('should format joke for twitter', async () => {
+    await getDadJokeMethod.process();
+    expect(dadJokeService.formatForTwitter).toHaveBeenCalledWith(joke);
+  });
+
+  test('should post joke to twitter', async () => {
+    await getDadJokeMethod.process();
+    expect(twitterService.postTweet).toHaveBeenCalledWith(jokeFormatted);
+  });
+
+  test('should get tweet url', async () => {
+    await getDadJokeMethod.process();
+    expect(twitterService.buildLinkToTweet).toHaveBeenCalledWith(tweet);
   });
 });
